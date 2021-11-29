8159347553a93c3780dffb4564db1a24e84c5e7c
emrecncelik <emrecncelik@gmail.com>
2021-11-29 13:22:01 +0300

    get env activate as input

--- run_nli.sh
+++ run_nli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source $FINETUNE_ENV_ACTIVATE_DIR
+source $1
 
 declare -a MODELS=("dbmdz/distilbert-base-turkish-cased" "dbmdz/bert-base-turkish-cased" "dbmdz/convbert-base-turkish-mc4-cased" )
 declare -a DATASETS=("snli_tr" "multinli_tr" )
@@ -10,7 +10,7 @@ do
     do
         python finetune-nli.py --model $MODEL \
                             --dataset $DATASET \
-                            --batch_size 16 \
-                            --output_dir /home/emrecan/tez/zeroshot-turkish/models \
+                            --batch_size 32 \
+                            --output_dir /home/emrecan/tez/zeroshot-turkish/models
     done
 done
