254ac7b621b8acb616d0d0c3128f7ec8c93c396a
emrecncelik <emrecncelik@gmail.com>
2022-06-21 16:19:13 +0300

    move format results to utils

--- zeroshot_classification/utils.py
+++ zeroshot_classification/utils.py
@@ -69,6 +69,51 @@ def deserialize(filename: str):
         return pickle.load(f)
 
 
+def format_results(result_path: str, metric: str = "accuracy", averaging: str = None):
+    # nli = format_results('/home/emrecan/tez/zeroshot-turkish/results/big/nli_results_final.bin', "accuracy")
+    # nsp = format_results('/home/emrecan/tez/zeroshot-turkish/results/big/nsp_results_final.bin', "accuracy")
+    # df = nli.join(nsp).round(3)
+
+    results = deserialize(result_path)
+    # [‘index’, ‘columns’, ‘data’, ‘index_names’, ‘column_names’].
+    columns = []
+    index = []
+    data = []
+    index_names = ["dataset", "template"]
+    column_names = ["model"]
+
+    for model, dict1 in results.items():
+        columns.append(model)
+        for dataset, dict2 in dict1.items():
+            for template, dict3 in dict2.items():
+                if (dataset, template) not in index:
+                    index.append((dataset, template))
+                    if metric in dict3["classification_report"]:
+                        data.append([dict3["classification_report"][metric]])
+                    else:
+                        data.append([dict3["classification_report"][averaging][metric]])
+                else:
+                    if metric in dict3["classification_report"]:
+                        data[index.index((dataset, template))].append(
+                            dict3["classification_report"][metric]
+                        )
+                    else:
+                        data[index.index((dataset, template))].append(
+                            dict3["classification_report"][averaging][metric]
+                        )
+
+    return pd.DataFrame.from_dict(
+        {
+            "columns": columns,
+            "index": index,
+            "data": data,
+            "column_names": column_names,
+            "index_names": index_names,
+        },
+        orient="tight",
+    )
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     # format_txt_dataset(os.path.join(DATA_PATH, "ruh_hali"), encoding="cp1254")
